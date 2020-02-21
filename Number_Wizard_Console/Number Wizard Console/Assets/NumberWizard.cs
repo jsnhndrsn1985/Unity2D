0510f665cdfb419d56adc061882a2030739d3d2f
jsnhndrsn1985 <jsnhndrsn1985@gmail.com>
2020-02-20 21:41:08 -0500

    First 3 projects

---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to Number Wizard.");
+        Debug.Log("Pick a Number");
+        Debug.Log($"The highest number you can pick is: {max}");
+        Debug.Log($"The lowest number you can pick is: {min}");
+        Debug.Log($"Tell me if your number is higher or lower than {guess}");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }       
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log($"I guessed it right! {guess} is your number!");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log($"Is it higher or lower than {guess} ?");
+    }
+}
